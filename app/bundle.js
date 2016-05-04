84079e574a258ddcd489877584f5ab8b2b6cbf97
Cristian Marcel <lcristianiim@yahoo.com>
2016-05-04 09:16:46 +0300

    Draw shadows

--- app/bundle.js
+++ app/bundle.js
@@ -17,6 +17,7 @@ var color = '';
 initTetris.init(grid, 'tetris-container');
 
 getPiece();
+console.log(piece.name);
 
 function getPiece() {
     color = colors[Math.floor(Math.random()*colors.length)];
@@ -28,21 +29,64 @@ function getPiece() {
     piece.init(grid, color);
 }
 
-// function drawShadow() {
-//     for (var i = 0; i < piece.grid.rows * piece.grid.columns; i++) {
-//         var block = document.getElementsByClassName('column');
-//         for (i = 0; i < block.length; i++) {
-//             if () {
-//             }
-//             block[i].style.backgroundColor = 'green';
-//         }
-//     }
-// }
-//
-// drawShadow();
-var me = helper.getPointFromBlock(10, grid);
-console.log(me);
+function drawShadow() {
+    for (var i = 0; i < piece.grid.rows * piece.grid.columns; i++) {
+        var block = document.getElementsByClassName('column');
+        for (i = 0; i < block.length; i++) {
+            if (block[i].getAttribute("used") == "used") {
+                var point = helper.getPointFromBlock(i, grid);
+                if (point.x - 1 >= 0) {
+                    for (var k = point.x - 1; k <= point.x + 1; k++ ) {
+                        if (point.y - 1 >= 0) {
+                            for (var t = point.y - 1; t <= point.y + 1; t++) {
+                                if (block[i].getAttribute("used") == "used" && t <= 10) {
+                                    var pointer = {
+                                        x: k,
+                                        y: t
+                                    }
+                                    var blocker = helper.getBlock(pointer, grid);
+                                    if (blocker) {
+                                        blocker.style.backgroundColor = 'black';
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
+
+function clearShadow() {
+    for (var i = 0; i < piece.grid.rows * piece.grid.columns; i++) {
+        var block = document.getElementsByClassName('column');
+        for (i = 0; i < block.length; i++) {
+            if (block[i].getAttribute("used") == "used") {
+                var point = helper.getPointFromBlock(i, grid);
+                if (point.x - 1 >= 0) {
+                    for (var k = point.x - 1; k <= point.x + 1; k++ ) {
+                        if (point.y - 1 >= 0) {
+                            for (var t = point.y - 1; t <= point.y + 1; t++) {
+                                if (block[i].getAttribute("used") == "used" && t <= 10) {
+                                    var pointer = {
+                                        x: k,
+                                        y: t
+                                    }
+                                    var blocker = helper.getBlock(pointer, grid);
+                                    if (blocker) {
+                                        blocker.style.backgroundColor = '#3C3E3C';
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
 
 // Handling the keydown event
@@ -50,28 +94,40 @@ document.body.onkeydown = function (event) {
 
     // up key
     if (event.keyCode == 38) {
+        clearShadow();
         piece.rotate();
+        drawShadow();
+        piece.drawShape();
     }
 
     // down key
     if (event.keyCode == 40) {
         if (check.down(piece)) {
+            clearShadow();
             piece.moveDown();
-            check.downCollision(piece, grid);
+            drawShadow();
+            piece.drawShape();
+            // check.downCollision(piece, grid);
         } else getPiece();
     }
 
     // left key
     if (event.keyCode == 37) {
         if (check.left(piece)) {
+            clearShadow();
             piece.moveLeft();
+            drawShadow();
+            piece.drawShape();
         }
     }
 
     // right key
     if (event.keyCode == 39) {
         if (check.right(piece)) {
+            clearShadow();
             piece.moveRight();
+            drawShadow();
+            piece.drawShape();
         }
     }
 }
@@ -156,7 +212,8 @@ function getPointFromBlock (blockNumber, grid) {
         y: ''
     };
 
-    point.x = blockNumber / grid.columns;
+    var number = (blockNumber % grid.columns)- 10;
+    point.x = Math.floor(blockNumber / grid.columns);
     point.y = blockNumber % grid.columns;
     return point;
 }
