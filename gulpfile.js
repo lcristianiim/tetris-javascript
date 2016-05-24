ead7f5ebe698692986c3caad4d054844aba24746
Cristian Marcel <lcristianiim@yahoo.com>
2016-05-24 18:16:34 +0300

    Gulpfile modified

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,10 @@ gulp.task('bundle-modules', function() {
     return browserify('./app/main.js')
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('.'));
+    .pipe(gulp.dest('./app/'));
 });
 
 gulp.task('default', function () {
     gulp.watch('lib/*js', ['bundle-modules']);
     gulp.watch('app/main.js', ['bundle-modules']);
 });
-
