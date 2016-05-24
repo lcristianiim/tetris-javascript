e1a985cc3e0b85b20c823aa7d219987419e34b75
Cristian Marcel <lcristianiim@yahoo.com>
2016-05-24 17:50:44 +0300

    Gulp modified

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
-const gulp = require('gulp'),
+const
+    gulp = require('gulp'),
+    source = require('vinyl-source-stream'),
     browserify = require('browserify');
 
 gulp.task('bundle-modules', function() {
     return browserify('./app/main.js')
+    .bundle()
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('.'));
-})
+});
+
+gulp.task('default', function () {
+    gulp.watch('lib/*js', ['bundle-modules']);
+    gulp.watch('app/main.js', ['bundle-modules']);
+});
+
